6c0f4017396d6946fb928defb3c29e729e34d686
Krasimir Konstantinov <ext_kkonstantinov@bigpoint.net>
2023-10-14 15:27:14 +0300

    #1 Fix .sql config and directory conflict

--- Database/schema.sql
+++ Database/schema.sql
@@ -4,10 +4,9 @@ CREATE TABLE IF NOT EXISTS BrainBox_lobbies (
     lobbyId INT AUTO_INCREMENT PRIMARY KEY,
     creatorUserId INT NOT NULL,
     secondUserId INT,
-    creationTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    creationTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 );
 
-CREATE USER 'BrainboxDefault'@'%' IDENTIFIED BY 'add_password_here';
 GRANT SELECT, INSERT, UPDATE, DELETE ON BrainBox.BrainBox_lobbies TO 'BrainboxDefault'@'%';
 FLUSH PRIVILEGES;
