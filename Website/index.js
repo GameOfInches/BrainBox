29e00d2c875379201214eafaf05cf7e8b8156247
ext_kkonstantinov <ext_kkonstantinov@bigpoint.net>
2023-10-16 10:39:19 +0300

    #3 remove https redirect when ssl certificate is not present

--- Website/index.js
+++ Website/index.js
@@ -1,25 +1,23 @@
-const https = require('https');
 const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-const httpsPort = 443;
-
 const hasCertificates = fs.existsSync('/app/certificates/key.pem') && fs.existsSync('/app/certificates/cert.pem');
 
-if(hasCertificates){
-const options =
-  {
-      key: fs.readFileSync('/app/certificates/key.pem', 'utf8'),
-      cert: fs.readFileSync('/app/certificates/cert.pem', 'utf8')
-    }
-  
-  const httpsServer = https.createServer(options, app);
-  
-  app.get('/', (req, res) => {
-    res.write("<h1>Hello World!</h1>");
+if (hasCertificates) {
+  const https = require('https');
+  const httpsPort = 443;
 
+  const options = {
+    key: fs.readFileSync('/app/certificates/key.pem', 'utf8'),
+    cert: fs.readFileSync('/app/certificates/cert.pem', 'utf8')
+  }
+
+  const httpsServer = https.createServer(options, app);
+
+  app.get('/', (req, res) => {
+    res.write("<h1>Hello World / HTTPS!</h1>");
     res.end();
   });
 
@@ -27,15 +25,15 @@ const options =
     console.log(`HTTPS server is running on port ${httpsPort}`);
   });
 } else {
-    const http = require("http");
-    const httpPort = 8080;
-  
-    const httpServer = http.createServer((req, res) => {
-      res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-      res.end();
-    });
-  
-    httpServer.listen(httpPort, () => {
-      console.log(`HTTP server is running on port ${httpPort}`);
-    });
-  }
+  const http = require("http");
+  const httpPort = 8080;
+
+  const httpServer = http.createServer((req, res) => {
+    res.write("<h1>Hello World / HTTP!</h1>");
+    res.end();
+  });
+
+  httpServer.listen(httpPort, () => {
+    console.log(`HTTP server is running on port ${httpPort}`);
+  });
+}
