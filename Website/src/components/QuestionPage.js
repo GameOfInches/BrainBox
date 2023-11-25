74daa5a5e7bb400a3dbf2445ff214aa7cfdc30b7
ArkiBuser <imperialpants@gmail.com>
2023-11-25 20:42:26 +0200

    logic for the questions type and audio implemented

--- Website/src/components/QuestionPage.js
+++ Website/src/components/QuestionPage.js
@@ -3,9 +3,15 @@ import '../App.css';
 import Timer from './Timer';
 import AnswerPage from './AnswerPage';
 import ReactPlayer from 'react-player';
+import useSound from 'use-sound';
+import Audio1 from '../Assets/Audio_1.mp3';
 const QuestionPage = ({lobbyId, username, roundNumber, setRoundNumber, toNewRound, setToNewRound}) => {
     const [timeLeft, setTimeLeft] = useState(10);
     const [timeOut, setTimeOut] = useState(false);
+    const [play, { stop }] = useSound(Audio1);
+    //placeholder for questiontype
+    const [questionType, setQuestionType] = useState("audio");
+
 
     //TODO: Fetch from the database the question and the image/video/audio and the type of the content
     
@@ -13,10 +19,15 @@ const QuestionPage = ({lobbyId, username, roundNumber, setRoundNumber, toNewRoun
     useEffect(() => {
       // Start the timer countdown
       const countdownInterval = setInterval(() => {
+        //this has to be changed when we get time per question from the database
+        if (timeLeft == 9) {
+          play();
+        }
         if (timeLeft > 0) {
           setTimeLeft(timeLeft - 1);
         } else {
           clearInterval(countdownInterval);
+          stop();
           setTimeOut(true);
         }
       }, 1000);
@@ -37,43 +48,38 @@ const QuestionPage = ({lobbyId, username, roundNumber, setRoundNumber, toNewRoun
 
 
     //for video playing
-    const videoSetting = {
-      height: '390',
-      width: '640',
-      playerVars: {
-        autoplay: 0,
-      },
-    };
-  
-    const onReady = (event) => {
-      // access to player in all event handlers via event.target
-      event.target.pauseVideo();
-    };
     /* */
   
     return (
+      
       <div className="question-card">
         <div>Round {roundNumber}</div>
-       
-        {timeOut ? <AnswerPage lobbyId = {lobbyId} username = {username} roundNumber = {roundNumber} setRoundNumber = {setRoundNumber} toNewRound = {toNewRound} setToNewRound = {setToNewRound}/> : <><div className="">
+        {timeOut ? <AnswerPage username = {username} roundNumber = {roundNumber} setRoundNumber = {setRoundNumber} toNewRound = {toNewRound} setToNewRound = {setToNewRound}/> : <><div className="">
             <Timer initialTime={10 * 1000} isPlaying={true} />
           </div>
         <div className="logo">
           <img src="planet.png" alt="Logo" />
         </div>
-        
-        <div className="title">Memorize the card, {username}...</div>
-        <div 
-        className="image-container">
-        <ReactPlayer
-        url="<https://youtu.be/vjhL4XS_6wY>"
-        width="640"
-        height="360"
-        playing={true}
-        controls
-      />
-          <img src="card.jpg" alt="Card" />
-        </div></>}
+        <div className="title">Memorize the contents, {username}...</div>
+        {questionType === "audio" ? (
+          <div className="audio-container">
+             <img src="volume.png" alt="Sound icon" />
+          </div>
+        ) : questionType === "image" ? (
+          <div className="image-container">
+            <img src="card.jpg" alt="Card" />
+          </div>
+        ) : (
+          <div className="video-container">
+            <ReactPlayer
+              url="https://youtu.be/vjhL4XS_6wY"
+              width="360"
+              height="720"
+              playing={true}
+              controls
+            />
+          </div>
+        )}</>}
         
       </div>
          
